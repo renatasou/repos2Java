3a056832698282afaf176d36c2949fcf6925d5b7
renatsume <renatsume@gmail.com>
2013-02-04 13:37:00 -0300

    alteracao da variavel numero e inclusao do metodo pagamento

--- projetoTeste/src/Cadastro_Usuario.java
+++ projetoTeste/src/Cadastro_Usuario.java
@@ -4,6 +4,10 @@ public class Cadastro_Usuario {
 	public int num = 0;
 	public String nome = "renata DOIDA demais";
 	
-	public int matAluno= 11111;
+	public int matAluno= 2222;
+	
+	public void pagamento (int valor){
+		
+	}
 
 }

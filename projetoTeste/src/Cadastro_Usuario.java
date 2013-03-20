06e470948aa1143b6695a3000c3deb5de1f0e2d1
renatsume <renatsume@gmail.com>
2013-03-20 10:28:59 -0300

    add o sysoutprintln de teste

--- projetoTeste/src/Cadastro_Usuario.java
+++ projetoTeste/src/Cadastro_Usuario.java
@@ -2,11 +2,13 @@
 public class Cadastro_Usuario {
 	
 	public int num = 0;
-	public String nome = "";
+	public String nome = "renata DOIDA demais";
 	
 	public int matAluno= 2222;
 	
 	public void pagamento (int valor){
+		
+		System.out.println("modificação para testar s foi para master");
 		
 	}
 
